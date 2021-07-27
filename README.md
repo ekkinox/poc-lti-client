4eed4bf18fff666b0b33b1affdf3596445e88d92
Jonathan Vuillemin <ekkinox@gmail.com>
2021-07-27 15:47:55 +0200

    feat: poc

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # poc-lti-client
+
+> Proof of concept to test how [LtiServiceClient](https://github.com/oat-sa/lib-lti1p3-core/blob/master/src/Service/Client/LtiServiceClient.php) based clients can have their behaviour extended via guzzle middlewares.
+
+To test:
+
+```shell
+vendor/bin/phpunit
+```
---
+++ tests/ServiceClientTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Ekkinox\PocLtiClient\Tests;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Request;
+use OAT\Library\Lti1p3Ags\Service\Score\Client\ScoreServiceClient;
+use OAT\Library\Lti1p3Ags\Tests\Traits\AgsDomainTestingTrait;
+use OAT\Library\Lti1p3Core\Service\Client\LtiServiceClient;
+use OAT\Library\Lti1p3Core\Tests\Traits\DomainTestingTrait;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\RequestInterface;
+use Throwable;
+
+class ServiceClientTest extends TestCase
+{
+    use DomainTestingTrait;
+    use AgsDomainTestingTrait;
+
+    /** @var array */
+    private $history;
+
+    /** @var ScoreServiceClient */
+    private $client;
+
+    protected function setUp(): void
+    {
+        $this->history = [];
+
+        $handlerStack = HandlerStack::create();
+
+
+        $handlerStack->push(
+            Middleware::mapRequest(function (RequestInterface $request) {
+                return $request->withHeader('X-Foo', 'bar');
+            })
+        );
+        $handlerStack->push(
+            Middleware::history($this->history)
+        );
+
+        $guzzleClient = new Client(['handler' => $handlerStack]);
+        $ltiServiceClient = new LtiServiceClient(null, $guzzleClient);
+        $this->client = new ScoreServiceClient($ltiServiceClient);
+    }
+
+    public function testHeaders(): void
+    {
+        $registration = $this->createTestRegistration();
+        $lineItem = $this->createTestLineItem();
+        $score = $this->createTestScore();
+
+        try {
+            $this->client->publishScore($registration, $score, $lineItem->getUrl());
+        } catch (Throwable $exception) {
+            // noop
+        }
+
+        /** @var Request $sentRequest */
+        $sentRequest = current($this->history)['request'];
+
+        $this->assertEquals('bar', $sentRequest->getHeaderLine('X-Foo'));
+    }
+}
